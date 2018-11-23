a6cdfc2f171000c4add37a3e87a06fbab24c698f
N.Gayathri <32363990+gayathriadityan1981@users.noreply.github.com>
2018-11-23 12:16:30 +0530

    Update CustomerFormValidator.java

--- src/main/java/com/shopping/foodorder/validator/CustomerFormValidator.java
+++ src/main/java/com/shopping/foodorder/validator/CustomerFormValidator.java
@@ -1,37 +1,37 @@
 package com.shopping.foodorder.validator;
  
 import org.apache.commons.validator.routines.EmailValidator;
-import com.shopping.foodorder.model.CustomerInfo;
+import com.shopping.foodorder.form.CustomerForm;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
- 
+
 // @Component: As a Bean.
 @Component
 public class CustomerFormValidator implements Validator {
  
-    private EmailValidator emailValidator = EmailValidator.getInstance();
+   private EmailValidator emailValidator = EmailValidator.getInstance();
  
-    // This Validator support CustomerInfo class.
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return clazz.equals(CustomerForm.class);
-    }
+   // This validator only checks for the CustomerForm.
+   @Override
+   public boolean supports(Class<?> clazz) {
+      return clazz == CustomerForm.class;
+   }
  
-    @Override
-    public void validate(Object target, Errors errors) {
-        CustomerInfo custInfo = (CustomerInfo) target;
+   @Override
+   public void validate(Object target, Errors errors) {
+      CustomerForm custInfo = (CustomerForm) target;
  
-        // Check the fields of CustomerInfo class.
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.customerForm.name");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty.customerForm.email");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "NotEmpty.customerForm.address");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phone", "NotEmpty.customerForm.phone");
+      // Check the fields of CustomerForm.
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.customerForm.name");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty.customerForm.email");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "NotEmpty.customerForm.address");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phone", "NotEmpty.customerForm.phone");
  
-        if (!emailValidator.isValid(custInfo.getEmail())) {
-            errors.rejectValue("email", "Pattern.customerForm.email");
-        }
-    }
+      if (!emailValidator.isValid(custInfo.getEmail())) {
+         errors.rejectValue("email", "Pattern.customerForm.email");
+      }
+   }
  
 }
