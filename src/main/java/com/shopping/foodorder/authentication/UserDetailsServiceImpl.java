2039a495756d7db9160812fc626db5f59aab83a6
N.Gayathri <32363990+gayathriadityan1981@users.noreply.github.com>
2018-11-23 10:39:37 +0530

    Update UserDetailsServiceImpl.java

--- src/main/java/com/shopping/foodorder/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/shopping/foodorder/authentication/UserDetailsServiceImpl.java
@@ -37,7 +37,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
  
         // ROLE_EMPLOYEE, ROLE_MANAGER
-        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+        GrantedAuthority authority = new SimpleGrantedAuthority(role);
  
         grantList.add(authority);
  
